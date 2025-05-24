230bfd441031b979552fa5aef34632a514450954
SahilKhudaniya <167081456+SahilKhudaniya@users.noreply.github.com>
2025-05-25 01:25:54 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,80 @@
+/*=============== MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close');
+
+/* Menu show */
+navToggle.addEventListener('click', () => {
+  navMenu.classList.add('show-menu');
+});
+
+/* Menu hidden */
+navClose.addEventListener('click', () => {
+  navMenu.classList.remove('show-menu');
+});
+
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+const scrollHeader = () => {
+  const header = document.getElementById('header');
+
+  this.scrollY >= 50
+    ? header.classList.add('bg-header')
+    : header.classList.remove('bg-header');
+};
+
+window.addEventListener('scroll', scrollHeader);
+
+
+/*=============== HOME HEIGHT ===============*/
+const homeHeight = () => {
+  const home = document.getElementById('home');
+  const mockup = document.getElementById('home-mockup');
+
+  home.style.height = `${mockup.offsetHeight + 313}px`;
+};
+
+window.addEventListener('load', homeHeight);
+window.addEventListener('resize', homeHeight);
+
+
+/*=============== FIRST SWIPER ===============*/
+var firstSwiper = new Swiper('.first-swiper', {
+  loop: true,
+  speed: 1000,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  on: {
+    slideChange: function () {
+      const slideIndex = this.realIndex;
+      const isAtFirstSlide = slideIndex === 0;
+      const left = isAtFirstSlide ? 0.25 : slideIndex * 50;
+
+      const mockupImage = document.getElementById('home-mockup');
+      mockupImage.style.setProperty('--left', `${left}%`);
+    },
+  },
+});
+
+/*=============== SECOND SWIPER ===============*/
+var secondSwiper = new Swiper('.second-swiper', {
+  loop: true,
+  direction: 'vertical',
+});
+/*=============== SYNCHRONIZING SWIPERS ===============*/
+firstSwiper.controller.control = secondSwiper;
+
+
+/*=============== GSAP ANIMATION ===============*/
+gsap.from('.home-title-animate', {
+  x: 1000,
+  duration: 1,
+  ease: 'power1.in',
+});
+
+gsap.from('.home-title-animate', {
+  y: 1000,
+  duration: 1,
+  ease: 'power1.in',
+});
